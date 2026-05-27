380a019f5ce789d3c2bbd9d915ee9dc46ef3329b
Zoey-Puffy <ajiang13260@gmail.com>
2026-05-27 22:37:33 +0800

    6.6 correction: Exercise: Building the login API

--- src/main/java/com/codewithmosh/store/controllers/AuthController.java
+++ src/main/java/com/codewithmosh/store/controllers/AuthController.java
@@ -1,7 +1,8 @@
 package com.codewithmosh.store.controllers;
 
-import com.codewithmosh.store.dtos.LoginDto;
+import com.codewithmosh.store.dtos.LoginRequest;
 import com.codewithmosh.store.repositories.UserRepository;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,8 +12,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
-
 @AllArgsConstructor
 @RestController
 @RequestMapping("/auth")
@@ -20,19 +19,15 @@ public class AuthController {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
-    @PostMapping("/auth/login")
-    public ResponseEntity<?> login(@RequestBody LoginDto loginDto) {
-        var user = userRepository.findUserByEmail(loginDto.getEmail());
+    @PostMapping("/login")
+    public ResponseEntity<Void> login(@Valid @RequestBody LoginRequest request) {
+        var user = userRepository.findByEmail(request.getEmail()).orElse(null);
         if (user == null) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("user", "not found"));
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
 
-        var storedPassword = passwordEncoder.encode(user.getPassword());
-        var reportPassword = passwordEncoder.encode(loginDto.getPassword());
-
-
-        if(!storedPassword.equals(reportPassword)) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(Map.of("password", "unauthorized"));
+        if(!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
 
         return ResponseEntity.ok().build();
