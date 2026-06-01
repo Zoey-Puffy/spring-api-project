c24f416f72ce0b98b727d5d2153d80e4c8c26183
Zoey-Puffy <ajiang13260@gmail.com>
2026-06-01 22:00:04 +0800

    7.3 correction: Creating entities

--- src/main/java/com/codewithmosh/store/entities/Order.java
+++ src/main/java/com/codewithmosh/store/entities/Order.java
@@ -1,10 +1,16 @@
 package com.codewithmosh.store.entities;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.math.BigDecimal;
-import java.util.Date;
+import java.time.LocalDateTime;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "orders")
 public class Order {
@@ -13,16 +19,20 @@ public class Order {
     @Column(name = "id")
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne
     @JoinColumn(name = "customer_id")
-    private User user;
+    private User customer;
 
     @Column(name = "status")
-    private String status;
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
 
-    @Column(name = "created_at")
-    private Date createdAt;
+    @Column(name = "created_at", insertable = false, updatable = false)
+    private LocalDateTime createdAt;
 
     @Column(name = "total_price")
     private BigDecimal totalPrice;
+
+    @OneToMany(mappedBy = "order")
+    private Set<OrderItem> items = new LinkedHashSet<>();
 }
--- src/main/java/com/codewithmosh/store/entities/OrderItem.java
+++ src/main/java/com/codewithmosh/store/entities/OrderItem.java
@@ -1,7 +1,13 @@
 package com.codewithmosh.store.entities;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.math.BigDecimal;
+
+@Getter
+@Setter
 @Entity
 @Table(name = "order_items")
 public class OrderItem {
@@ -10,20 +16,20 @@ public class OrderItem {
     @Column(name = "id")
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne
     @JoinColumn(name = "order_id")
     private Order order;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne
     @JoinColumn(name = "product_id")
     private Product product;
 
     @Column(name = "unit_price")
-    private Double unitPrice;
+    private BigDecimal unitPrice;
 
     @Column(name = "quantity")
     private Integer quantity;
 
     @Column(name = "total_price")
-    private Double totalPrice;
+    private BigDecimal totalPrice;
 }
